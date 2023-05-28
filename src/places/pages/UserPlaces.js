675c0a6896d804eb0fe91b5f071816f487daa074
Mohamed Thameem <thameemsh@gmail.com>
2023-05-27 23:58:48 -0400

    Create places list and item components

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Buidling',
+        imageUrl:"https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
+        address: '20 W 34th St, New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878584
+        },
+        creator: 'u1'
+    },
+    {
+        id: 'p2',
+        title: 'Empire State Buidling',
+        imageUrl:"https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
+        address: '20 W 34th St, New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878584
+        },
+        creator: 'u2'
+    }
+    
+];
+const UserPlaces = () => {
+ return <PlaceList items={DUMMY_PLACES}/>;
+}
+
+export default UserPlaces;
--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -7,24 +7,24 @@ import Backdrop from "../UIElements/Backdrop";
 import "./MainNavigation.css";
 const MainNavigation = (props) => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
-  const openDrawer = () => {
+  const openDrawerHandler = () => {
     setDrawerIsOpen(true);
   };
-  const closeDrawer = () => {
+  const closeDrawerHandler = () => {
     setDrawerIsOpen(false);
   };
   return (
     <React.Fragment>
-      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
 
-        <SideDrawer show={drawerIsOpen} onClick={closeDrawer}>
+        <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
           <nav className="main-navigation__drawer-nav">
             <NavLinks />
           </nav>
         </SideDrawer>
    
       <MainHeader>
-        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+        <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
           <span />
           <span />
           <span />
