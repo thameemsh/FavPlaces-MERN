809e3d4b586a606e6d7789389d31a5b325a103c4
Mohamed Thameem <thameemsh@gmail.com>
2023-05-28 00:06:15 -0400

    Filter all places based on userId in UserPlaces component

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PlaceList from '../components/PlaceList';
+import { useParams } from 'react-router-dom';
 
 const DUMMY_PLACES = [
     {
@@ -27,7 +28,9 @@ const DUMMY_PLACES = [
     
 ];
 const UserPlaces = () => {
- return <PlaceList items={DUMMY_PLACES}/>;
+    const userId = useParams().userId;
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
+ return <PlaceList items={loadedPlaces}/>;
 }
 
 export default UserPlaces;
